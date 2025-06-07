a4b1cffe49f95c84f63def8a69fe016e77fe3767
Llorente14 <axel.crisdy1980@gmail.com>
2025-06-07 16:35:56 +0700

    fix/redirectHometoOrder

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\SelfOrder; // Asumsikan nama komponennya SelfOrder
 use App\Livewire\ViewOrder;
 use App\Livewire\Payment;
+
+//Redirect langsung ke order bagi user
+Route::redirect('/', '/order'); 
 Route::get('/order', SelfOrder::class)->name('self-order');
 Route::get('/view-orders', ViewOrder::class);
 Route::get('/payment', Payment::class);
